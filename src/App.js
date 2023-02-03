bb4c57ef10f115c1c885c9fb7260f3ac65a8d8cf
gor.gharagyozyan <gor.gharagyozyan@softconstruct.com>
2023-02-03 17:49:13 +0400

    Initial.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,161 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/style/style.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const quarterHour = 40;
+    const eventDuration = 2;
+
+    return (
+        <div className='schedule'>
+            <div className='schedule-in'>
+                <div className='schedule-timeline'>
+                    <ul>
+                        <li>
+                            <span>09:00</span>
+                        </li>
+                        <li>
+                            <span>09:15</span>
+                        </li>
+                        <li>
+                            <span>09:30</span>
+                        </li>
+                    </ul>
+                </div>
+                <div className='schedule-content'>
+                    <div className='schedule-column'>
+                        <div className='schedule-header'>
+                            <h4 className='schedule-header-in'>Entry</h4>
+                        </div>
+                        <div className='schedule-body'>
+                            <div className='schedule-item'
+                                 style={{height: (eventDuration * quarterHour) / 16 + 'rem'}}>
+                                <div className='schedule-item-in'>
+                                    <div className='schedule-item-title'>
+                                        <h6 className='schedule-item-title-in'>Registration</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='schedule-in'>
+                <div className='schedule-timeline'>
+                    <ul>
+                        <li>
+                            <span>09:45</span>
+                        </li>
+                        <li>
+                            <span>10:00</span>
+                        </li>
+                        <li>
+                            <span>10:15</span>
+                        </li>
+                        <li>
+                            <span>10:30</span>
+                        </li>
+                        <li>
+                            <span>10:45</span>
+                        </li>
+                        <li>
+                            <span>11:00</span>
+                        </li>
+                    </ul>
+                </div>
+                <div className='schedule-content'>
+                    <div className='schedule-column'>
+                        <div className='schedule-header'>
+                            <h4 className='schedule-header-in'>3rd Hall</h4>
+                        </div>
+                        <div className='schedule-body'>
+                            <div className='schedule-item'
+                                 style={{height: (eventDuration * quarterHour) / 16 + 'rem'}}>
+                                <div className='schedule-item-in'>
+                                    <div className='schedule-item-title'>
+                                        <h6 className='schedule-item-title-in'>Topic 4</h6>
+                                    </div>
+                                    <div className='schedule-item-info'>
+                                        <ul className='schedule-item-info-in'>
+                                            <li>
+                                                <span className='language en'>en</span>
+                                            </li>
+                                            <li>
+                                                <span className='level beginner'/>
+                                            </li>
+                                        </ul>
+                                        <div className='schedule-item-speaker'>
+                                            <span className='schedule-item-speaker-name'>Davit Matevosyan</span>
+                                            <img className='schedule-item-speaker-image'
+                                                 src="https://via.placeholder.com/22x22" alt="no photo"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='schedule-column'>
+                        <div className='schedule-header'>
+                            <h4 className='schedule-header-in'>3rd Hall</h4>
+                        </div>
+                        <div className='schedule-body'>
+                            <div className='schedule-item'
+                                 style={{height: (eventDuration * quarterHour) / 16 + 'rem'}}>
+                                <div className='schedule-item-in'>
+                                    <div className='schedule-item-title'>
+                                        <h6 className='schedule-item-title-in'>Topic 4</h6>
+                                    </div>
+                                    <div className='schedule-item-info'>
+                                        <ul className='schedule-item-info-in'>
+                                            <li>
+                                                <span className='language en'>en</span>
+                                            </li>
+                                            <li>
+                                                <span className='level beginner'/>
+                                            </li>
+                                        </ul>
+                                        <div className='schedule-item-speaker'>
+                                            <span className='schedule-item-speaker-name'>Davit Matevosyan</span>
+                                            <img className='schedule-item-speaker-image'
+                                                 src="https://via.placeholder.com/22x22" alt="no photo"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='schedule-column'>
+                        <div className='schedule-header'>
+                            <h4 className='schedule-header-in'>3rd Hall</h4>
+                        </div>
+                        <div className='schedule-body'>
+                            <div className='schedule-item'
+                                 style={{height: (eventDuration * quarterHour) / 16 + 'rem'}}>
+                                <div className='schedule-item-in'>
+                                    <div className='schedule-item-title'>
+                                        <h6 className='schedule-item-title-in'>Topic 4</h6>
+                                    </div>
+                                    <div className='schedule-item-info'>
+                                        <ul className='schedule-item-info-in'>
+                                            <li>
+                                                <span className='language en'>en</span>
+                                            </li>
+                                            <li>
+                                                <span className='level beginner'/>
+                                            </li>
+                                        </ul>
+                                        <div className='schedule-item-speaker'>
+                                            <span className='schedule-item-speaker-name'>Davit Matevosyan</span>
+                                            <img className='schedule-item-speaker-image'
+                                                 src="https://via.placeholder.com/22x22" alt="no photo"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
